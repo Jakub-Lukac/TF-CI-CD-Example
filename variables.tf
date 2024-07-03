1e0a161cdaa88082a8f7a0a7f57edfc6dca8b6f6
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-07-03 11:42:01 +0200

    variables modified

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "env_client_id" {
   type = string
+  default = ""
 }
 
 variable "env_client_secret" {
   type = string
+  default = ""
 }
 
 variable "env_subscription_id" {
   type = string
+  default = ""
 }
 
 variable "env_tenant_id" {
   type = string
+  default = ""
 }
