c4e6200fc1eac742412f68c72b9408a372072425
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-07-03 10:58:13 +0200

    azure cli login added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "env_client_id" {
+  type = string
+}
+
+variable "env_client_secret" {
+  type = string
+}
+
+variable "env_subscription_id" {
+  type = string
+}
+
+variable "env_tenant_id" {
+  type = string
+}
