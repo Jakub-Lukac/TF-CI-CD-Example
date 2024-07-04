091d8b5fb8dccc2a6e9f6eabff4fb2deca238485
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-07-04 12:43:57 +0200

    main notes added

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = var.resource_group_name     # name will wary depending on the .tfvars files used 
+  location = var.resource_group_location # location will wary depending on the .tfvars files used 
 
   lifecycle {
     ignore_changes = [tags]
